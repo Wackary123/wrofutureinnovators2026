89292eaa6faa7b5d97ea232027469810eace6c14
alexwithadog <alex.zhu@e.sainteanne.ca>
2026-04-11 10:30:01 -0400

    .

--- voicetesting.py
+++ voicetesting.py
@@ -6,11 +6,11 @@ tts = Espeak()
 # Set amplitude 0-200, default 100
 tts.set_amp(200)
 # Set speed 80-260, default 150
-tts.set_speed(80)
+tts.set_speed(260)
 # Set gap 0-200, default 1
-tts.set_gap(2)
+tts.set_gap(200)
 # Set pitch 0-99, default 80
-tts.set_pitch(50)
+tts.set_pitch(1)
 
 tts.say("Hello! I’m Espeak TTS.")
 """
